1fa9aa89ff2daf4cc4a43f917512963bf9a4ab6b
Lukas Henkel <lh@entf.net>
2024-12-03 06:15:56 +0100

    Add test

--- t/day-3.lisp
+++ t/day-3.lisp
@@ -4,4 +4,5 @@
 
 (define-test test-day-3
     ()
-  )
+  (assert= 161 (aoc:run-day 3 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
+  (assert= 48 (nth-value 1 (aoc:run-day 3 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"))))
