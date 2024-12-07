1af7fd644bbac6ad0c08078a59d37b095fbeca10
Lukas Henkel <lh@entf.net>
2024-12-07 07:42:22 +0100

    Inline some math

--- src/day-7.lisp
+++ src/day-7.lisp
@@ -3,24 +3,31 @@
   (:export #:day-7))
 (in-package #:aoc/day-7)
 
+(declaim (inline number-digits combine)
+         (ftype (function (fixnum) fixnum) number-digits)
+         (ftype (function (fixnum fixnum) fixnum) combine))
+
 (defun number-digits (number)
   (if (zerop number)
       1
       (1+ (floor (log number 10)))))
 
 (defun combine (num-1 num-2)
-  (+ (* num-1 (expt 10 (number-digits num-2))) num-2))
+  (+ (the fixnum (* num-1
+                    (the fixnum (expt 10 (number-digits num-2)))))
+     num-2))
 
 (defun test-value-valid-p (test-value numbers &optional combine-op)
   (labels ((%solve (test-value numbers combine-op current)
+             (declare (type fixnum current test-value))
              (if (null numbers)
                  (= current test-value)
                  (if (> current test-value)
                      nil
                      (or (%solve test-value (cdr numbers) combine-op
-                                 (+ current (car numbers)))
+                                 (+ current (the fixnum (car numbers))))
                          (%solve test-value (cdr numbers) combine-op
-                                 (* current (car numbers)))
+                                 (* current (the fixnum (car numbers))))
                          (and combine-op
                               (%solve test-value (cdr numbers) combine-op
                                       (combine current (car numbers)))))))))
